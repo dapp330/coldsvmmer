68ccea3235ac527b7de1f9435d9b699b8bd4a079
dapp330 <daffaaditya901@gmail.com>
2023-01-05 22:33:03 +0700

    add lazy, suspense

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 import "tachyons";
 import "./image.js";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { NavbarComponent } from "./components/Headers/headers";
-import { ContentComponent } from "./components/Content_Component";
-import { Test } from "./components/test";
 import { AboutPage } from "./components/about_page";
 import { FooterComponent } from "./components/footer/footer";
 import { GalleryComponent } from "./components/gallery_page";
+
+const ContentComponent = lazy(() => import("./components/Content_Component"));
 
 const App = () => {
   return (
     <Router>
       <div class="flex flex-column">
-        <div>
-          <NavbarComponent />
-        </div>
-        <div>
-          <Routes>
-            <Route path="/" element={<ContentComponent />} />
-            <Route path="gallery" element={<GalleryComponent />} />
-            <Route path="about" element={<AboutPage />} />
-          </Routes>
-        </div>
-        <div>
-          <FooterComponent />
-        </div>
+        <Suspense
+          fallback={
+            <span
+              class="center white mt7 center bl bt br bb"
+              style={{ fontFamily: "dancing script", fontSize: "25vh" }}
+            >
+              ColdSvmmer
+            </span>
+          }
+        >
+          <div>
+            <NavbarComponent />
+          </div>
+          <div>
+            <Routes>
+              <Route path="/" element={<ContentComponent />} />
+              <Route path="gallery" element={<GalleryComponent />} />
+              <Route path="about" element={<AboutPage />} />
+            </Routes>
+          </div>
+          <div>
+            <FooterComponent />
+          </div>
+        </Suspense>
       </div>
     </Router>
   );
