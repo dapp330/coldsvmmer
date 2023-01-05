e6b5394cbdb42426e165d35fcb4ed17d95f81c3e
dapp330 <daffaaditya901@gmail.com>
2023-01-05 20:47:10 +0700

    new card

--- src/components/Content_Component.js
+++ src/components/Content_Component.js
@@ -1,14 +1,14 @@
-import { normalizeStyle } from "@ant-design/cssinjs/lib/hooks/useStyleRegister";
-import React, { Suspense } from "react";
-import LazyLoad from "react-lazy-load";
+import { Card } from "antd";
+import Meta from "antd/es/card/Meta";
+import React from "react";
 
 const logo1 = require("../assets/pic/5.png");
 const foto1 = require("../assets/pic/sutit.png");
 const foto2 = require("../assets/pic/vokal.png");
 const foto3 = require("../assets/pic/gitar.png");
 
-const gallery = () => {
-  // const images = [foto1, foto2, foto3];
+//home page picture
+const cards = () => {
   const anggota = [
     {
       foto: foto1,
@@ -32,38 +32,16 @@ const gallery = () => {
 
   return anggota.map((ag) => {
     return (
-      <div
-        class="w-30 mr3 ml4"
-        style={{
-          background: `url(${ag.foto})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div>
-          <a href={ag.instagram}>
-            <div class="box center flex">
-              <div class="card ma5 mt7">
-                <div class="imgBx">
-                  <img src={ag.foto} alt="images" />
-                </div>
-                <div class="details">
-                  <h2>
-                    {ag.nama}
-                    <br />
-                    <span>{ag.posisi}</span>
-                  </h2>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
+      <div class="flex center w-30">
+        <Card cover={<img src={ag.foto} style={{ height: "25rem" }} />}>
+          <Meta style={{}} title={ag.nama} description={ag.posisi} />
+        </Card>
       </div>
     );
   });
 };
 
+//video player
 const players = () => {
   const videos = [
     {
@@ -91,23 +69,10 @@ const players = () => {
   return videos.map((video) => {
     return (
       <div class="ml3 mr3 w-40 h-100 mb3 mt3">
-        <iframe src={video.link} class="db w-100 video-card" loading="lazy" />
+        <iframe src={video.link} class="db w-100 video-card" />
         <span class="b center f3 white-70 fw3">{video.judul}</span>
       </div>
     );
-  });
-};
-
-const song = () => {
-  const songs = [
-    {
-      judul: "Michaela",
-      link: "https://open.spotify.com/embed/artist/138waJFOBaoYmIcpe8ckSI?utm_source=generator",
-    },
-  ];
-
-  return songs.map((sg) => {
-    return <img src={sg.link} class="br4 h3 w3 dib"></img>;
   });
 };
 
@@ -133,12 +98,11 @@ const ContentComponent = () => {
             </h1>
           </div>
         </header>
-        <div class=" vh-100 w-100 flex flex-row mt3">{gallery()}</div>
+        <div class=" vh-100 w-100 flex flex-row mt3">{cards()}</div>
         <div class="cf justify-center flex flex-wrap mt5">{players()}</div>
-        <div class="pa4 tc">{song()}</div>
       </article>
     </body>
   );
 };
 
-export { ContentComponent };
+export default ContentComponent;
